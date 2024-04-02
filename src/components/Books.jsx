468050cf9e4e28e92ffb6e2fbdddf3f2dfa2d5f0
Federico Peirano <fedepr2345@gmail.com>
2024-04-02 16:14:32 -0300

    d. Iniciar Sesion y Actualizar la Cache - Ejercicios8.19

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,12 +1,49 @@
 import PropTypes from 'prop-types';
 import Book from './Book'
+import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_GENRES = gql`
+query {
+  allGenres
+}
+`
 
 const Books = (props) => {
+  const genres = useQuery(ALL_GENRES, {
+    pollInterval: 2000
+  })
+  const [genre, setGenre] = useState()
+  const books = props.books
+
+
   if (!props.show) {
     return null
   }
 
-  const books = props.books
+  const showBook = () => {
+    if(!genre) {
+      console.log('Entre en genre undefined')
+      return books.map((b) => {
+        console.log(b)
+        return(
+          <tr key={b.title}>
+            <Book key={b.title} book={b}/>
+          </tr>
+        )
+      })
+    } else {
+      console.log('Entre en genre para filtrar')
+      return books.map((b) => (
+        b.genres.includes(genre) ? (
+          <tr key={b.title}>
+            <Book key={b.title} book={b}/>
+          </tr>
+        ) : null
+      ))
+    }
+  }
+
 
   return (
     <div>
@@ -19,13 +56,16 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((b) => (
-            <tr key={b.title}>
-              <Book key={b.title} book={b}/>
-            </tr>
-          ))}
+          {showBook()}
         </tbody>
       </table>
+      <div>
+        {
+          genres.data.allGenres.map((g) =>(
+            <button key={g} onClick={() => setGenre(g)}>{g}</button>
+          ))
+        }
+      </div>
     </div>
   )
 }
